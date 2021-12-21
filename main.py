e56836ae57d4963c15260e178d04a5471e12a20a
Rafał Raźny <rafal-razny@wp.pl>
2021-12-21 19:59:18 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+list_fives = []
+for i in range (1,101):
+    if i % 5 == 0:
+        list_fives.append(i)
+print (list_fives)
+
+list_power_of_three = [ z ** 3 for z in list_fives]
+print (list_power_of_three)
